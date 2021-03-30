dd3c1f0314a39e80dcb6aa27e3eccc462110ff6a
bavanDA <56412698+bavanDA@users.noreply.github.com>
2021-03-30 20:49:04 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
         <div className="outer">
           <div className="inner">
-            <Grid container spacing={0}>
+            <Grid container spacing={3}>
               <Grid item xs={8}>
                 <Switch>
                   <Route exact path="/" component={Login} />
