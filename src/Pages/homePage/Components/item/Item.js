a400f40e6487ab18726136d2921717012efc6260
mr3 <mr3@gmail.com>
2021-04-02 02:56:04 +0430

    owl-Carousels-Added

--- src/Pages/homePage/Components/item/Item.js
+++ src/Pages/homePage/Components/item/Item.js
@@ -1,9 +1,24 @@
 import "./Item.css"
+import eminem from "./eminem.jpg"
 
 function Item(props){
     return(
         <div class="bod">
-            <div class="card"></div>
+            <div class="card">
+                <div class="container">
+                    <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|} r1">
+                        
+                    </div>
+                    <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|} r2">
+                        <img src={eminem}></img>
+                    </div>
+                    <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|} r3">
+                        <div class="titles">8 Miles</div>
+                        <div class="disc">Eminem</div>
+                        <div class="rate">Rate : 4.6</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
