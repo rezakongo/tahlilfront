d13866405d85c19d93ab1c1377e6c3395d8b970a
mr3 <mr3@gmail.com>
2021-04-03 21:04:16 +0430

    HomePage OKgit add -A!

--- src/Pages/homePage/HomePage.js
+++ src/Pages/homePage/HomePage.js
@@ -1,58 +1,96 @@
-
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import OwlCarousel from 'react-owl-carousel'
+import 'owl.carousel/dist/assets/owl.carousel.min.css'
+import 'owl.carousel/dist/assets/owl.theme.default.min.css'
+import './HomePage.css';
 import Nav from './Components/Navbar/Navbar'
+import Item from "./Components/item/Item"
 
 function HomePage() {
   
+  const Album={
+    title:'8 Mile',
+    artist:'Eminem',
+    rating:4.5,
+    pic:'./item/eminem.jpg',
+  }
   return (
     <div>
-    <Nav></Nav>
-    <div className="bdy" class="container" >
-      <div className="slideContainer" class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
-        <div className="slideBar">
-          SlideShow
+      <Nav></Nav>
+      <div className="bdy" class="container" >
+        <div className="slideContainer" class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+        <div><h1 class="titles">New Musics</h1></div>
+          <div className="slideBar">
+          <OwlCarousel className='d-none d-xl-block  owl-theme' items="6" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-none d-xl-none d-md-block owl-theme' items="4" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-md-none owl-theme' items="3" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          </div>
         </div>
-      </div>
-      <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-        <div className="Tops">
-              Top10 Musics
+        <div className="slideContainer" class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+          <div><h1 class="titles">Top 10 Musics</h1></div>
+          <div className="slideBar">
+          <OwlCarousel className='d-none d-xl-block  owl-theme' items="6" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-none d-xl-none d-md-block owl-theme' items="4" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-md-none owl-theme' items="3" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          </div>
+        </div>
+        <div className="slideContainer" class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+          <div><h1 class="titles">Top 10 Albums</h1></div>
+          <div className="slideBar">
+          <OwlCarousel className='d-none d-xl-block  owl-theme' items="6" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-none d-xl-none d-md-block owl-theme' items="4" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-md-none owl-theme' items="3" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          </div>
+        </div>
+        <div className="slideContainer" class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+          <div><h1 class="titles">Top 10 Artists</h1></div>
+          <div className="slideBar">
+          <OwlCarousel className='d-none d-xl-block  owl-theme' items="6" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-none d-xl-none d-md-block owl-theme' items="4" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          <OwlCarousel className='d-md-none owl-theme' items="3" loop margin={1}>
+            <Item></Item>
+          </OwlCarousel>
+          </div>
+        </div>
+      </div> 
+      <div className="Foot">
+        <div class="container-fluid !direction !spacing">
+          <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+            <div class="col-md-4 col-sm-12">
+              Provided by HexClan Team
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-        <div className="Tops">
-              Top10 Musics
+            <div class="col-md-4 col-sm-12">
+              مطابق قوانین جمهوری اسلامی ایران 
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-        <div className="Tops">
-              Top10 Musics
+            <div class="col-md-4 col-sm-12">
+              پیوند ها 
             </div>
+          </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-        <div className="Tops">
-              Top10 Musics
-            </div>
-        </div>
-      </div>
+      </div>  
     </div>
-    <div className="Foot">
-      <div class="container-fluid !direction !spacing">
-        <div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
-          <div class="col-md-4 col-sm-12">
-            Provided by HexClan Team
-          </div>
-          <div class="col-md-4 col-sm-12">
-            مطابق قوانین جمهوری اسلامی ایران 
-          </div>
-          <div class="col-md-4 col-sm-12">
-            پیوند ها 
-          </div>
-        </div>
-      </div>
-    </div>  
-  </div>
   );
 }
 
