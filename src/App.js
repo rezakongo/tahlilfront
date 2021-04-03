df6c0ca1f5f0e473c62f09a72d79793b1eda456f
Bavan Divaaniazar <bavan1379@gamil.com>
2021-04-03 22:12:26 +0430

    final version

--- src/App.js
+++ src/App.js
@@ -1,89 +1,20 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import test from "./Cover.jpg";
 import "./App.css";
-import 'fontsource-roboto';
-
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import Login from "./Components/login";
 import SignUp from "./Components/signup";
-import { Col, Row } from "react-bootstrap";
-import { Container, Grid, Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
 
 function App() {
-  const classes = useStyles();
-
   return (
     <Router>
       <div className="bg-image"></div>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/signin"}></Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/signin"}>
-                    Sign in
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/signup"}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="outer">
-          <div className="inner">
-<<<<<<< HEAD
-            <Grid container spacing={0}>
-              <Grid item xs={1}>
-=======
-            <Grid container spacing={3}>
-              <Grid item xs={8}>
->>>>>>> dd3c1f0314a39e80dcb6aa27e3eccc462110ff6a
-                <Switch>
-                  <Route exact path="/" component={Login} />
-                  <Route path="/signin" component={Login} />
-                  <Route path="/signup" component={SignUp} />
-                </Switch>
-              </Grid>
-<<<<<<< HEAD
-=======
-              <Grid item xs={3}>
-                <img src={test} alt="test" className="img"></img>
-              </Grid>
->>>>>>> dd3c1f0314a39e80dcb6aa27e3eccc462110ff6a
-            </Grid>
-
-            {/* <Container>
-              <Row>
-                <Col xs={3} md={7}>
-              
-                </Col>
-                <Col xs={1} md={5} style={{ backgroundColor: "#c5c  c7" }}>
-                </Col>
-              </Row>
-            </Container> */}
-          </div>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
       </div>
     </Router>
   );
--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -5,27 +5,6 @@ import { Visibility, VisibilityOff } from "@material-ui/icons";
 import { withStyles } from "@material-ui/core/styles";
 import "./login.css";
 
-const CssTextField = withStyles({
-  root: {
-    "& label.Mui-focused": {
-      color: "grey",
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "grey",
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "red",
-      },
-      "&:hover fieldset": {
-        borderColor: "grey",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "grey",
-      },
-    },
-  },
-})(TextField);
 export default class SignUp extends Component {
   state = {
     showPassword: false,
@@ -37,90 +16,101 @@ export default class SignUp extends Component {
     };
 
     return (
-      <form className="form">
-        <h3 className="text">Register</h3>
+      <div className="container">
+        <div className="padding"></div>
+        <div className="row justify-content-center">
+          <div className="col-xs-7 col-md-6 col-lg-5">
+            <div className="outer">
+              <div className="inner">
+                <form className="form">
+                  <h3 className="text">Register</h3>
 
-        <div className="form-group">
-          <TextField
-            id="firstname"
-            label="First Name"
-            margin="normal"
-            required
-            variant="filled"
-            className="input"
-          />
-        </div>
+                  <div className="form-group">
+                    <TextField
+                      id="firstname"
+                      label="First Name"
+                      margin="normal"
+                      required
+                      variant="filled"
+                      className="input"
+                    />
+                  </div>
 
-        <div className="form-group">
-          <TextField
-            id="lastname"
-            label="Last Name"
-            margin="normal"
-            required
-            variant="filled"
-            className="input"
-          />
-        </div>
+                  <div className="form-group">
+                    <TextField
+                      id="lastname"
+                      label="Last Name"
+                      margin="normal"
+                      required
+                      variant="filled"
+                      className="input"
+                    />
+                  </div>
 
-        <div className="form-group">
-          <TextField
-            id="email"
-            label="Email"
-            margin="normal"
-            required
-            variant="filled"
-            autoComplete="email"
-            className="input"
-          />
-        </div>
+                  <div className="form-group">
+                    <TextField
+                      id="email"
+                      label="Email"
+                      margin="normal"
+                      required
+                      variant="filled"
+                      autoComplete="email"
+                      className="input"
+                    />
+                  </div>
 
-        <div className="form-group">
-          <CssTextField
-            label="Password"
-            margin="normal"
-            required
-            variant="filled"
-            className="input"
-            type={this.state.showPassword ? "text" : "password"} // <-- This is where the magic happens
-            InputProps={{
-              // <-- This is where the toggle button is added.
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleClickShowPassword}
+                  <div className="form-group">
+                    <TextField
+                      label="Password"
+                      margin="normal"
+                      required
+                      variant="filled"
+                      className="input"
+                      type={this.state.showPassword ? "text" : "password"} // <-- This is where the magic happens
+                      InputProps={{
+                        // <-- This is where the toggle button is added.
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleClickShowPassword}
+                            >
+                              {this.state.showPassword ? (
+                                <Visibility />
+                              ) : (
+                                <VisibilityOff />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="btn btn-dark btn-lg btn-block"
+                    style={{
+                      backgroundColor: "#C5C6C7",
+                      color: "#45A29E",
+                      fontWeight: "bold",
+                      borderRadius: "10px",
+                      marginBottom: "10px",
+                    }}
                   >
-                    {this.state.showPassword ? (
-                      <Visibility />
-                    ) : (
-                      <VisibilityOff />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+                    Register
+                  </button>
+
+                  <p className="forgot-password text-right">
+                    <a href="login">Already registered log in?</a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <button
-          type="submit"
-          className="btn btn-dark btn-lg btn-block"
-          style={{
-            backgroundColor: "#C5C6C7",
-            color: "#45A29E",
-            fontWeight: "bold",
-            borderRadius: "10px",
-            marginBottom: "10px",
-          }}
-        >
-          Register
-        </button>
-
-        <p className="forgot-password text-right">
-          <a href="#">Already registered log in?</a>
-        </p>
-      </form>
+      </div>
     );
   }
 }
