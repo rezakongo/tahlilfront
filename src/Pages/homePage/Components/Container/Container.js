7303be7d30403ac7f4a326ec47a23972859a73f4
mr3 <mr3@gmail.com>
2021-04-11 22:38:47 +0430

    Album_Cards_Added

--- src/Pages/homePage/Components/Container/Container.js
+++ src/Pages/homePage/Components/Container/Container.js
@@ -1,63 +1,44 @@
 import React from "react";
 import "./Container.css";
-
+import HomePageAlbum from "./Cards/HomePageAlbum"
 
 function Container(){
     return(
         <div className="containerBody">
             <div className="container-fluid !direction !spacing">
-                <div className="row align-items-start">
-                    
-                </div>
                 <div className="row align-items-center ContainerRows">
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                 </div>
                 <div className="row align-items-center ContainerRows">
                     <div  className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                 </div>
                 <div className="row align-items-center top-buffer ContainerRows">
                     <div  className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        <HomePageAlbum></HomePageAlbum>
                     </div>
                     <div className="col-md-4 col-sm-12 containcells">
-                        <div className="CardContainer">
-                            
-                        </div>
+                        
+                        <HomePageAlbum></HomePageAlbum>
+                        
                     </div>
                 </div>
             </div>
