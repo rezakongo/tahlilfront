942f192c767efe1b17bbf71c2eaf4adf7b1e238e
mr3 <mr3@gmail.com>
2021-04-12 23:45:26 +0430

    Carousels_Added

---
+++ src/Components/AtistsCarousel/ArtistsCarousel.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./ArtistsCarousel.css"
+import HomePageArtist from "./Cards/homePageArtist"
+
+export default class ArtistsCarousel extends Component {
+  render() {
+    const settings = {
+        slidesToShow: 3,
+        infinite: true,
+        dots:false,
+      responsive: [
+        
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 2,
+                infinite: true,
+                dots:false,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 1,
+                infinite: true,
+                dots:false,
+            }
+        },
+      ]
+    };
+    return (
+        <div className="CarouselContainerTemplate">
+            <div className="CarouselTemplate">
+                <h2> Artists</h2>
+                <Slider {...settings} >
+                <div>
+                    <HomePageArtist/>
+                </div>
+                <div>
+                    <HomePageArtist/>
+                </div>
+                <div>
+                    <HomePageArtist/>
+                </div>
+                <div>
+                    <HomePageArtist/>
+                </div>
+                <div>
+                    <HomePageArtist/>
+                </div>
+                <div>
+                    <HomePageArtist/>
+                </div>
+                </Slider>
+            </div>
+        </div>
+    );
+  }
+}
