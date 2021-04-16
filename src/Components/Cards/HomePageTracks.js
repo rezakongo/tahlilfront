767c40a0f5994dcf506200bafcde498e3a54bf2d
Bavan Divaaniazar <bavan1379@gamil.com>
2021-04-16 20:49:05 +0430

    Track's bug need to be fixed

--- src/Components/Cards/HomePageTracks.js
+++ src/Components/Cards/HomePageTracks.js
@@ -1,214 +1,42 @@
 import React from "react";
 import Rating from "@material-ui/lab/Rating";
 import { Table, Image } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Cover from "./cover-dsotm.jpg";
-import './HomePageTracks.css';
+import "./HomePageTracks.css";
+import { TracksData } from "./TracksData";
 
 const TableExampleUnstackable = () => (
   <Table className="table" unstackable inverted>
     <Table.Body>
-      <Table.Row>
-        <Table.Cell>1</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Speak to Me</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={3.8}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>2</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Breathe</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.2}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>3</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>On the Run</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>4</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Time</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.7}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>5</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>The Great Gig in the Sky</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.4}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>6</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Money</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.4}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>7</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Us and Them</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.5}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>8</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Any Colour You Like</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>9</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Brain Damage</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.5}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>10</Table.Cell>
-        <Table.Cell>
-          <Image
-            src={Cover}
-            rounded
-            size="mini"
-          />
-        </Table.Cell>
-        <Table.Cell>Eclipse</Table.Cell>
-        <Table.Cell>Pink Floyd</Table.Cell>
-        <Table.Cell>
-          <Rating
-            value={4.1}
-            precision={0.1}
-            size={"small"}
-            readOnly={true}
-          ></Rating>
-        </Table.Cell>{" "}
-      </Table.Row>
+      {TracksData.map((track) => {
+        return (
+          <div>
+            <div className="row trackRow" id="trackRow" href="test">
+              <div class="col-sm">
+                <div className="row" id="trackRow2">
+                  <div class="col-sm-1 text">{track.num}</div>
+                  <div class="col-sm-2">
+                    <Image src={Cover} rounded size="mini" />
+                  </div>
+                  <div class="col-sm text">{track.trackName}</div>
+                  <div class="col-sm text">{track.singerName}</div>
+                  <div class="col-sm text">
+                    <Rating
+                      value={track.rateValue}
+                      precision={0.1}
+                      size={"small"}
+                      readOnly={true}
+                      style={{ verticalAlign: "middle" }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </Table.Body>
   </Table>
 );
