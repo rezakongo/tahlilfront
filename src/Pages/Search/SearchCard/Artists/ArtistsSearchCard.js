19950d1163e3740346d88a247d4b6e73daf0caee
Bavan Divaaniazar <bavan1379@gamil.com>
2021-05-27 21:42:39 +0430

    fix-merge 2

--- src/Pages/Search/SearchCard/Artists/ArtistsSearchCard.js
+++ src/Pages/Search/SearchCard/Artists/ArtistsSearchCard.js
@@ -9,31 +9,39 @@ function ArtistsSearchCard(props) {
       <div className="container-fluid !direction !spacing test">
         <div className="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
           <div className="col-md-12 col-sm-12  textContainer">
-            <img width="100%" height="185" id="trackImg" src={
-                (props.artist.photo=="")
+            <img
+              width="100%"
+              height="185"
+              id="trackImg"
+              src={
+                props.artist.photo == ""
                   ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPzxLjgPNZxJ_P_BH-k9DCKnd-StnfPdtnoA&usqp=CAU"
                   : props.artist.photo
-              } />
+              }
+            />
             <Header as="h2" id="TrackTitle">
-            {props.artist.name}
-              <Header.Subheader id="TrackTitle">{props.artist.type}</Header.Subheader>
+              {props.artist.name}
+              <Header.Subheader id="TrackTitle">
+                {props.artist.type}
+              </Header.Subheader>
             </Header>
             <h3 className="TrackScore">
-            <ReactStars
-                        count={5}
-                        size={20}
-                        value={props.artist.score}
-                        isHalf={true}
-                        emptyIcon={<i className="far fa-star"></i>}
-                        halfIcon={<i className="fa fa-star-half-alt"></i>}
-                        fullIcon={<i className="fa fa-star"></i>}
-                        activeColor="#fbff00"
-                    />
+              <ReactStars
+                count={5}
+                size={20}
+                value={props.artist.score}
+                isHalf={true}
+                emptyIcon={<i className="far fa-star"></i>}
+                halfIcon={<i className="fa fa-star-half-alt"></i>}
+                fullIcon={<i className="fa fa-star"></i>}
+                activeColor="#fbff00"
+              />
             </h3>
             <h3 className="TrackRelease">1973-09-03</h3>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
--- src/Pages/Search/SearchCard/Tracks/TracksSearchCard.js
+++ src/Pages/Search/SearchCard/Tracks/TracksSearchCard.js
@@ -10,12 +10,8 @@ function TracksSearchCard(props) {
       <div className="container-fluid !direction !spacing test">
         <div className="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
           <div className="col-md-12 col-sm-12  textContainer">
-<<<<<<< HEAD
-            <img width="250px" height="250px" id="trackImg" src={TrackImage} />
-=======
             <img width="100%" height="185" id="trackImg" src={TrackImage} />
-            
->>>>>>> c6916d54347730bb8429c616aad3ec951e007d23
+
             <Header as="h2" id="TrackTitle">
               {props.track.title}
               <Header.Subheader id="TrackTitle">
@@ -23,16 +19,16 @@ function TracksSearchCard(props) {
               </Header.Subheader>
             </Header>
             <h3 className="TrackScore">
-            <ReactStars
-                        count={5}
-                        size={20}
-                        value={props.track.score}
-                        isHalf={true}
-                        emptyIcon={<i className="far fa-star"></i>}
-                        halfIcon={<i className="fa fa-star-half-alt"></i>}
-                        fullIcon={<i className="fa fa-star"></i>}
-                        activeColor="#fbff00"
-                    />
+              <ReactStars
+                count={5}
+                size={20}
+                value={props.track.score}
+                isHalf={true}
+                emptyIcon={<i className="far fa-star"></i>}
+                halfIcon={<i className="fa fa-star-half-alt"></i>}
+                fullIcon={<i className="fa fa-star"></i>}
+                activeColor="#fbff00"
+              />
             </h3>
             <h3 className="TrackRelease">relese</h3>
           </div>
